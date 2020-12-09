272cce5a66c10148f4dbca1a2ef0d8206235ff1d
Artem Ryabkov <rad964@gmail.com>
2020-12-09 03:13:55 +0300

    Tests for splitting multiple failed tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -136,3 +136,47 @@ def test_list_failed_telegram(testdir):
         assert message_text == expected_text
         assert message_chat_id == telegram_chat_id
         assert send_sticker == fail_sticker_id
+
+def test_list_failed_with_dots_telegram(testdir):
+    """Make sure plugin sends limited amount of failed tests."""
+
+    testdir.makepyfile(
+        """
+        import pytest
+        
+        @pytest.mark.parametrize('id', range(10))
+        def test_fail(id):
+            assert 1 != 1
+        """
+    )
+
+    telegram_token = 'Token'
+    telegram_chat_id = '130559633'
+    telegram_report_url = 'http://report_link.com'
+    fail_sticker_id = 'CAACAgIAAxkBAAMIX8rohSxoNbodB1D38VZx9HI2CDwAAmIBAAIQGm0izcITZBkXtbceBA'
+    expected_text = 'Passed=0 Failed=10 Skipped=0 Error=0 XFailed=0 XPassed=0' \
+                    '\nhttp://report_link.com\n' \
+                    '\nFailed tests:' \
+                    '\ntest_list_failed_with_dots_telegram.py::test_fail[0]' \
+                    '\ntest_list_failed_with_dots_telegram.py::test_fail[1]' \
+                    '\ntest_list_failed_with_dots_telegram.py::test_fail[2]' \
+                    '\n...'
+    with mock.patch('requests.post') as mock_post:
+        testdir.runpytest('--telegram_id', telegram_chat_id,
+                          '--telegram_token', telegram_token,
+                          '--telegram_report_url', telegram_report_url,
+                          '--telegram_list_failed')
+
+        sticker_request = mock_post.mock_calls[0]
+        sticker_called_url = sticker_request[1][0]
+        send_sticker = sticker_request[2]['json']['sticker']
+        message_request = mock_post.mock_calls[4]
+        message_called_url = message_request[1][0]
+        message_text = message_request[2]['json']['text']
+        message_chat_id = message_request[2]['json']['chat_id']
+
+        assert sticker_called_url == f'https://api.telegram.org/bot{telegram_token}/sendSticker'
+        assert message_called_url == f'https://api.telegram.org/bot{telegram_token}/sendMessage'
+        assert message_text == expected_text
+        assert message_chat_id == telegram_chat_id
+        assert send_sticker == fail_sticker_id
